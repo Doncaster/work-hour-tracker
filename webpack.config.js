77d8a01efcc29193805cebee93945086cc49c29d
Leinonen Teemu <teemu.leinonen@polar.com>
2016-12-23 14:05:57 +0200

    Updated configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var argv = require('yargs').argv;
+var _ = require('lodash');
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var Webpack = require('webpack');
@@ -9,7 +11,7 @@ module.exports = {
     entry: {
         favicon: './public/favicon.ico',
         app: './src/index.jsx',
-        vendor: ['moment', 'firebase', 'lodash']
+        vendor: ['react', 'moment', 'firebase', 'lodash']
     },
     output: {
         path: './build',
@@ -52,26 +54,30 @@ module.exports = {
             }
         }]
     },
-    plugins: [
+    plugins: _.concat([
         new HtmlWebpackPlugin({
             template: 'public/index.html',
             inject: 'body',
             excludeChunks: ['favicon']
         }),
         new Webpack.optimize.CommonsChunkPlugin('vendor', 'js/vendor.bundle.js'),
-        new Webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
         new Webpack.DefinePlugin({
             FIREBASE_API_KEY: JSON.stringify(firebaseConfig.apiKey),
             FIREBASE_AUTH_DOMAIN: JSON.stringify(firebaseConfig.authDomain),
             FIREBASE_DB_URL: JSON.stringify(firebaseConfig.databaseUrl),
             FIREBASE_STORAGE_BUCKET: JSON.stringify(firebaseConfig.storageBucket),
-            FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(firebaseConfig.messagingSenderId)
+            FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(firebaseConfig.messagingSenderId),
+
+            'process.env': {
+                NODE_ENV: (argv.prod ? JSON.stringify('production') : null)
+            }
         })
-    ],
+    ], (argv.prod ? new Webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }) : [])
+    ),
     resolve: {
         extensions: ['.webpack.js', '.web.js', '.js', '/index.jsx', '']
     },
